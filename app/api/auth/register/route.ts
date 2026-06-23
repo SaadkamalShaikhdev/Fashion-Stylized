08d5ad38d75e562768c38d3ef66b0b5ad41555d1
Saad kamal <saadkamaldev@gmail.com>
2026-06-23 13:26:47 +0500

    Revert "Remove rate limit from regsiter route because it only it 1 time for otp"

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -4,7 +4,8 @@ import { connectToDatabase } from "@/lib/db";
 import User from "@/models/User"
 import { registerSchema } from "@/lib/register-validation/auth";
 import {sendOTPEmail} from "@/lib/resend"
-
+import { checkRateLimit } from "@/lib/checkRateLimit";
+import { otpRateLimit } from "@/lib/ratelimit"
 export const dynamic = "force-dynamic"
 
 export async function POST(request: NextRequest){
@@ -55,7 +56,8 @@ const validatedData = result.data;
    const savedUser = await newUser.save();
 
     // Step 7: send OTP email
-
+const rateLimitResponse = await checkRateLimit(request, otpRateLimit)
+  if (rateLimitResponse) return rateLimitResponse
     await sendOTPEmail({email: validatedData.email, username: validatedData.username, otp});
  
 
