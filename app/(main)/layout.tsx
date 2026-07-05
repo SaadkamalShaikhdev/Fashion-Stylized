225a7bc4c71a4960a3b0649e38961b7f4182960a
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2026-07-05 13:37:38 +0500

    Added: Banner

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Whatsapp from "../components/Whatsapp"
+import TopBanner from "../components/layout/TopBanner"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+    <TopBanner />
       <Navbar />
       <main className="flex-1">
         {children}
