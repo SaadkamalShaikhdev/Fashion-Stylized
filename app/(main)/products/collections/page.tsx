2bf67b9f4b4eadaf0c71da2edc20642590f6b403
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2026-07-06 22:45:03 +0500

    Changed: pages of collection

---
+++ app/(main)/products/collections/page.tsx
@@ -0,0 +1,32 @@
+import CollectionPages from "./CollectionDetails"
+
+export const metadata = {
+  title: "Premium Wallets, Watches & Glasses in Pakistan | Fashion Stylized",
+  description:
+    "Shop premium wallets, watches, and glasses in Pakistan at Fashion Stylized. Discover stylish accessories, modern designs, and quality craftsmanship for men and women.",
+
+  keywords: [
+    "wallets in Pakistan",
+    "watches in Pakistan",
+    "glasses in Pakistan",
+    "fashion accessories Pakistan",
+    "men wallets Pakistan",
+    "women wallets Pakistan",
+    "luxury watches Pakistan",
+    "eyewear Pakistan",
+    "online shopping Pakistan",
+    "Fashion Stylized Pakistan",
+  ],
+
+  openGraph: {
+    title: "Premium Wallets, Watches & Glasses in Pakistan | Fashion Stylized",
+    description:
+      "Explore premium wallets, watches, and glasses for men and women in Pakistan. Shop stylish accessories at Fashion Stylized.",
+    type: "website",
+    locale: "en_PK",
+  },
+};
+
+export default function Page() {
+  return <CollectionPages />
+}
